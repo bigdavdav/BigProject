90ced836d0df17424cfbe8981bf248103f55402c
Davi Almeida <davi.castelo.branco.almeida@gmail.com>
2025-04-23 10:46:09 +0100

    added most functionality to hero and added an additional image to it as well

---
+++ slideshow.js
@@ -0,0 +1,72 @@
+// --------------------------- Variables -------------------------------
+
+const heroImages = document.getElementsByClassName("image-container")
+const amountOfImages = heroImages.length
+const heroImagesArray = []
+
+let i = 0 // place in the array when looping it
+
+const leftArrow = document.getElementById("left-button")
+const rightArrow = document.getElementById("right-button")
+
+// --------------------------- Functions -------------------------------
+
+function cycleArray() {
+  if ( i == (amountOfImages - 1 ) ) {
+    i = 0
+  } else {
+    i++
+  }
+  
+  imageSwitchFowards()
+}
+
+function imageSwitchFowards() {
+  heroImagesArray[i].classList.toggle("hidden")
+
+  if ( i == 0 ) {
+    heroImagesArray[amountOfImages - 1].classList.toggle("hidden")
+  } else {
+    heroImagesArray[i - 1].classList.toggle("hidden")
+  }
+}
+
+function imageSwitchBackwards() {
+  heroImagesArray[i].classList.toggle("hidden")
+
+  if ( i == amountOfImages - 1 ) {
+    heroImagesArray[0].classList.toggle("hidden")
+  } else {
+    heroImagesArray[i + 1].classList.toggle("hidden")
+  }
+}
+
+const interval = setInterval(cycleArray, 5000)
+
+// ------------ Creating interval for image switching -----------------
+
+for ( let i = 0; i < amountOfImages; i++ ) {
+  heroImagesArray.push(heroImages[i])
+}
+
+// ---------------- Adding function to the buttons --------------------
+
+rightArrow.addEventListener("click", () => {
+  if ( i == (amountOfImages - 1 ) ) {
+    i = 0
+  } else {
+    i++
+  }
+
+  imageSwitchFowards()
+})
+
+leftArrow.addEventListener("click", () => {
+  if ( i == 0 ) {
+    i = (amountOfImages - 1 )
+  } else {
+    i--
+  }
+
+  imageSwitchBackwards()
+})
